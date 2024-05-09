3133643fcf4ae7b13f010ea7e0ec938c6f3ea0d7
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-10 00:54:10 +0200

    fix endpoint

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000
 
-//middleware
-app.use(express.urlencoded)
-app.use(express.json)
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
 
 //route
 app.get('/', (req, res) => {
