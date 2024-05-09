8ff0609a8914829285012862785f0b435b76a092
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-10 00:27:51 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const fs = require('fs');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(express.urlencoded)
+app.use(express.json)
+
+//route
+app.get('/', (req, res) => {
+    const { ping } = req.params
+    if(ping) res.json({message: ping})
+    res.json({message: "PONG"})
+})
+
+app.get('/details', (req, res) => {
+
+    const filename = 'package.json';
+    let lines = 0, words = 0, letters = 0;
+
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) res.status(500).json({message: err.message});
+
+        lines = data.split('\n').length;
+        words = data.split(/\s+/).length;
+        letters = data.replace(/\s/g, '').length;
+
+        console.log(`File ${filename} has been Processed with Lines: ${lines} Words: ${words} Letters: ${letters}`);
+        res.json({
+            lines: lines,
+            words: words,
+            letters: letters
+        })
+    });
+})
+
+app.listen(port, () => {
+  console.log(`file-reader-node application listening on port ${port}`)
+})
